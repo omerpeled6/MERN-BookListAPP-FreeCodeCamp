651e24a0d850d801da11d86d31e85a91081f141c
Omer Peled <121338114+omerpeled6@users.noreply.github.com>
2024-05-04 10:15:55 +0300

    Update confing.js

--- backend/confing.js
+++ backend/confing.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://opeled6:Omerpeled1@cluster0.xpgc2vv.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://<UserName>:<Password>@cluster0.xpgc2vv.mongodb.net/?retryWrites=true&w=majority';
 
 // dont forget to add the password
