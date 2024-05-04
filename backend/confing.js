01f727cd4b156d6ea39454e271cf8a0828c625e7
omerpeled6 <opeled6@gmail.com>
2024-05-04 10:11:42 +0300

    final

---
+++ backend/confing.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://opeled6:Omerpeled1@cluster0.xpgc2vv.mongodb.net/?retryWrites=true&w=majority';
+
+// dont forget to add the password
